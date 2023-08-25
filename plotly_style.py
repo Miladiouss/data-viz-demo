5f49ef6ec606510de5e30689efd72809b752577f
miladiouss <miladiouss@gmail.com>
2023-08-24 21:32:02 -0700

    content added

--- plotly_style.py
+++ plotly_style.py
@@ -11,7 +11,7 @@ Usage:
 import plotly.graph_objects as go
 
 
-def update_layout(fig: go.Figure):
+def update_layout(fig: go.Figure, grid=True, constant_legend_itemsizing=False):
     """Enhance appearance of plotly figures by updating its layout.
 
     Parameters
@@ -25,27 +25,34 @@ def update_layout(fig: go.Figure):
     fig.layout.legend.font.family = "monospace"
 
     # Set Font Size
-    fig.layout.font.size = 20
+    fig.layout.font.size = 16
     fig.layout.title.font.size = fig.layout.font.size
     fig.layout.xaxis.title.font.size = fig.layout.font.size
     fig.layout.yaxis.title.font.size = fig.layout.font.size
-    fig.layout.legend.font.size = fig.layout.font.size - 3
+    fig.layout.legend.font.size = fig.layout.font.size
 
     # Configure Plot Size
-    fig.layout.width = 850
+    fig.layout.width = 750
+    fig.layout.height = 500
     # Configure Colors
     fig.layout.plot_bgcolor = "rgba(0, 0, 0, 0)"
 
     # Configure Legend
-    fig.layout.legend.bgcolor = "rgba(0, 0, 0, 0)"
-    fig.layout.legend.itemsizing = "constant"
     fig.layout.showlegend = True
+    fig.layout.legend.bgcolor = "rgba(0, 0, 0, 0)"
+    if constant_legend_itemsizing:
+        fig.layout.legend.itemsizing = "constant"
+
+    # Legend Position
+    fig.layout.legend.xanchor = "right"
+    fig.layout.legend.x = 1
 
     # Set Margins
-    fig.layout.margin.t = 75
-    fig.layout.margin.b = 75
-    fig.layout.margin.l = 75
-    fig.layout.margin.r = 75
+    m = 75
+    fig.layout.margin.t = m
+    fig.layout.margin.b = m
+    fig.layout.margin.l = m
+    fig.layout.margin.r = m
     fig.layout.margin.pad = 0
 
     # Configure Axis Lines
@@ -61,17 +68,41 @@ def update_layout(fig: go.Figure):
     # Configure Ticks
     fig.layout.xaxis.ticks = "inside"
     fig.layout.yaxis.ticks = "inside"
-    fig.layout.xaxis.minor.ticks = "inside"
-    fig.layout.yaxis.minor.ticks = "inside"
-    fig.layout.xaxis.minor.ticklen = 2
-    fig.layout.yaxis.minor.ticklen = 2
+    # Configure Minor Ticks
+    if grid:
+        fig.layout.xaxis.minor.ticks = "inside"
+        fig.layout.yaxis.minor.ticks = "inside"
+        fig.layout.xaxis.minor.ticklen = 1
+        fig.layout.yaxis.minor.ticklen = 1
 
     # Configure Grid
     fig.layout.xaxis.gridcolor = "rgba(0, 0, 0, .15)"
     fig.layout.yaxis.gridcolor = "rgba(0, 0, 0, .15)"
     fig.layout.xaxis.zerolinecolor = "rgba(0, 0, 0, .15)"
     fig.layout.yaxis.zerolinecolor = "rgba(0, 0, 0, .15)"
-    fig.layout.xaxis.minor.showgrid = True
-    fig.layout.yaxis.minor.showgrid = True
+    fig.layout.xaxis.minor.showgrid = grid
+    fig.layout.yaxis.minor.showgrid = grid
+    fig.layout.xaxis.showgrid = grid
+    fig.layout.yaxis.showgrid = grid
     fig.layout.xaxis.minor.gridcolor = "rgba(1, 1, 1, .05)"
     fig.layout.yaxis.minor.gridcolor = "rgba(1, 1, 1, .05)"
+
+
+def add_caption(fig, text="Figure 1. Example Data Visualization."):
+    n_lines = len(text.split("<br>"))
+    fig.layout.margin.b += 20 * n_lines
+    fig.layout.height += 20 * n_lines
+    _ = fig.add_annotation(
+        dict(
+            xref="paper",
+            yref="paper",
+            xanchor="left",
+            yanchor="top",
+            align="left",
+            x=0,
+            y=-0.175,
+            showarrow=False,
+            text=text,
+            font=dict(size=16),
+        )
+    )
